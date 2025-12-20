ca51f30e08d7f63deae2938a73f9033b06d74f84
rcrawf13 <richmondcrawford44@gmail.com>
2025-12-19 19:53:16 -0500

    Added navbar component functionality and sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
-import NavBar from './components/NavBar'
+import HomeSec from './components/HomeSec'
+import NavBar from './components/navbar/NavBar'
+
 function App() {
   const navButtonOptions: string[] = ["Home","About Us", "Contact", "Pricing"]
 
+  
   return (
     <>
-      <NavBar navButtonOptions={navButtonOptions} />
+
+        <NavBar navButtonOptions={navButtonOptions} />
+        <HomeSec />
+        <section id='about'></section>
+        <section id='contact'></section>
+        <section id='pricing'></section>
+
     </>
   )
 }
---
+++ src/components/HomeSec.tsx
@@ -0,0 +1,14 @@
+const HomeSec = () => {
+  return (
+    <section id='home'>
+        <div className="splashText">
+            <h1>Nomad <br /> Adventure <br /> Rentals</h1>
+            <p>Affordable boat rentals for every adventure.</p>
+            <button>Book Now</button>
+        </div>
+    </section>
+
+  )
+}
+
+export default HomeSec
