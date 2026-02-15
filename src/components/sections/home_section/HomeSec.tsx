d6a221c91740bd78a785d74626d49ca2aa2eaa15
rcrawf13 <richmondcrawford44@gmail.com>
2026-02-14 20:42:20 -0500

    Added Dynamic subheading text with embla

--- src/components/sections/home_section/HomeSec.tsx
+++ src/components/sections/home_section/HomeSec.tsx
@@ -1,13 +1,29 @@
 import splashImage from "../../../assets/hero-text.svg";
 import BookingButton from "../../booking_button/BookingButton"
-const HomeSec = () => {
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from 'embla-carousel-autoplay';
+import Fade from 'embla-carousel-fade'
 
+const HomeSec = () => {
+  const [emblaRef] = useEmblaCarousel({loop:true},[Autoplay(),Fade()]);
   return (
     <>
       <div className="wallpaper"></div>
       <div draggable="false" className="splashItems">
           <img className="heroText" src={splashImage} alt="Nomad Adventure Rental" />
-          <p>Affordable boat rentals for every adventure.</p>
+            <div ref={emblaRef} className="textCarouselRoot">
+              <div className="textCarouselContainer">
+                <div id="p1">
+                  <p>Affordable boat rentals for every adventure.</p>
+                </div>
+                <div id="p2">
+                  <p>Your perfect pontoon getaway starts here.</p>
+                </div>
+                <div id="p3">
+                  <p>Smooth cruising, zero stress.</p>
+                </div>
+              </div>
+          </div>
           <BookingButton/>
         </div>
     </>
