c818dd5aa9300c81272fae91c2e15c454a7fca88
rcrawf13 <richmondcrawford44@gmail.com>
2026-02-14 20:41:27 -0500

    Stripe Payment Gateway component

---
+++ src/routes/checkout/Checkout.tsx
@@ -0,0 +1,24 @@
+// import {Elements} from '@stripe/react-stripe-js';
+// import {loadStripe} from '@stripe/stripe-js';
+import { useEffect } from 'react';
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+// const stripePromise = loadStripe('pk_live_51PcAMnAaMeD4c46g3LNNbKk94NZGHBwRVbJFnTHQUlf2rD8gABAVqSmebUpwGVPZd8gdpzla0SEtN0HcXR3WE69K00wyLSHnsK') // Needs Key
+const Checkout = () => {
+  useEffect(()=>{
+
+
+
+
+  },[])
+
+  // const options = {
+  //   clientSecret: ''
+  // }
+  return (
+      <>
+      </>
+  )
+}
+
+export default Checkout
